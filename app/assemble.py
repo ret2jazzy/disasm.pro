64af93aae7a9659d2f187fc2207fa25f943cbd60
drigg3r <20831187+drigg3r@users.noreply.github.com>
2019-04-04 04:00:48 -0500

    Added label support

--- app/assemble.py
+++ app/assemble.py
@@ -1,6 +1,7 @@
 from flask import session
 from . import ninja_socketio
 from .constants import keystone_modes
+from .disassemble import capstone_instances
 from keystone import *
 
 keystone_instances = {}
@@ -36,21 +37,36 @@ Receive a JSON Object with data to assemble
 def assemble(code):
     try:
         current_settings = session['settings']
+        
+        starting_offset = int(current_settings['OFFSET'])
+        current_offset = starting_offset 
+
+        #We are gonna do an hack to support labels, since I want line by line assembly and also support labels
+        #I'm gonna first assemble it all at once, and then assemble each line, catching any symbol exceptions for labels
+        #I'll then disassemble the original code at that point to get the the instruction that uses the label and add it
 
         current_keystone = keystone_instances[current_settings['ARCH']][current_settings['MODE']][current_settings['ENDIAN']]
+        current_capstone = capstone_instances[current_settings['ARCH']][current_settings['MODE']][current_settings['ENDIAN']]
+        current_capstone.detail = True
 
-        code_to_assemble = code['code'].strip().split("\n")
+        init_assembled_code = current_keystone.asm(code['code'],current_offset)[0] 
+
+        code_to_assemble_by_line = code['code'].strip().split("\n")
 
         assembled_code = []
 
-        current_offset = current_settings['OFFSET']
+        for each_line in code_to_assemble_by_line:
+            try:
+                machine_line = current_keystone.asm(each_line,  current_offset)[0]
+                if machine_line is None:
+                    machine_line = []
+            except keystone.KsError as e:#The line probably uses a label
+                current_assembled_code = bytes(init_assembled_code[current_offset-starting_offset:]) #Bytes cuz capstone only accepts bytestrings
+                machine_line = list(next(current_capstone.disasm(current_assembled_code, current_offset)).bytes) #next() cuz capstone returns a generator object
 
-        for each_line in code_to_assemble:
-            machine_line = current_keystone.asm(each_line,  int(current_offset))[0]
-            if machine_line is None:
-                machine_line = []
+            current_offset += len(machine_line) #The offset increases by length of assembled code 
             assembled_code.append(machine_line)
 
         ninja_socketio.emit('assembled', assembled_code)
     except:
-        return 
+        return
